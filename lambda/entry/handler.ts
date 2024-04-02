a7902286324b73c9e40f91a7f8884a3efc4bdf41
masaminh <masami.askw@gmail.com>
2024-04-02 11:58:46 +0900

    X-Rayトレース対応

--- lambda/entry/handler.ts
+++ lambda/entry/handler.ts
@@ -2,13 +2,21 @@ import 'source-map-support/register';
 import middy from '@middy/core';
 import { Logger } from '@aws-lambda-powertools/logger';
 import { injectLambdaContext } from '@aws-lambda-powertools/logger/middleware';
+import { Tracer } from '@aws-lambda-powertools/tracer';
+import { captureLambdaHandler } from '@aws-lambda-powertools/tracer/middleware';
 import * as app from './app';
+
+const serviceName = 'notify_horse_infos';
 
 const logger = new Logger({
   logLevel: 'INFO',
-  serviceName: 'notify_horse_infos',
+  serviceName,
 });
+
+const tracer = new Tracer({ serviceName });
 
 app.setLogger(logger);
 // eslint-disable-next-line import/prefer-default-export
-export const handler = middy(app.entryPoint).use(injectLambdaContext(logger));
+export const handler = middy(app.entryPoint)
+  .use(injectLambdaContext(logger))
+  .use(captureLambdaHandler(tracer));
