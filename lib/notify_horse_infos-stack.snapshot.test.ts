b01d3d6cffec35c75b64af9b3f6aa3106c72b426
masaminh <masami.askw@gmail.com>
2025-09-27 16:45:59 +0900

    S3Key正規化ロジックを追加

--- lib/notify_horse_infos-stack.snapshot.test.ts
+++ lib/notify_horse_infos-stack.snapshot.test.ts
@@ -3,13 +3,45 @@ import { App } from 'aws-cdk-lib';
 import { NotifyHorseInfosStack } from './notify_horse_infos-stack';
 import { ContextType } from './context_type';
 
+// S3Keyのハッシュ値を正規化するためのヘルパー関数
+function normalizeS3Keys(template: any): any {
+  const normalized = JSON.parse(JSON.stringify(template));
+
+  // S3Keyの値を正規化
+  function normalizeObject(obj: any): any {
+    if (typeof obj === 'object' && obj !== null) {
+      if (Array.isArray(obj)) {
+        return obj.map(normalizeObject);
+      }
+      const normalizedObj: any = {};
+      Object.entries(obj).forEach(([key, value]) => {
+        if (key === 'S3Key' && typeof value === 'string') {
+          // S3Keyのハッシュ部分を固定値に置換
+          normalizedObj[key] = value.replace(/[a-f0-9]{64}/g, 'HASH_PLACEHOLDER');
+        } else {
+          normalizedObj[key] = normalizeObject(value);
+        }
+      });
+      return normalizedObj;
+    }
+    return obj;
+  }
+
+  return normalizeObject(normalized);
+}
+
 describe('NotifyHorseInfosStack Snapshot Tests', () => {
   let app: App;
   let stack: NotifyHorseInfosStack;
   let context: ContextType;
 
   beforeEach(() => {
-    app = new App();
+    app = new App({
+      context: {
+        // CDKのアセットハッシュを固定化してテスト環境での一貫性を確保
+        '@aws-cdk/aws-lambda-nodejs:useLatestRuntimeVersion': false,
+      },
+    });
     context = {
       stackName: 'test-stack',
       horsesJsonBucket: 'test-horses-bucket',
@@ -28,37 +60,43 @@ describe('NotifyHorseInfosStack Snapshot Tests', () => {
 
   test('should match snapshot for complete stack', () => {
     const template = Template.fromStack(stack);
-    expect(template.toJSON()).toMatchSnapshot();
+    const normalizedTemplate = normalizeS3Keys(template.toJSON());
+    expect(normalizedTemplate).toMatchSnapshot();
   });
 
   test('should match snapshot for Lambda function configuration', () => {
     const template = Template.fromStack(stack);
     const lambdaFunctions = template.findResources('AWS::Lambda::Function');
-    expect(lambdaFunctions).toMatchSnapshot();
+    const normalizedLambdaFunctions = normalizeS3Keys(lambdaFunctions);
+    expect(normalizedLambdaFunctions).toMatchSnapshot();
   });
 
   test('should match snapshot for Step Functions state machine', () => {
     const template = Template.fromStack(stack);
     const stateMachines = template.findResources('AWS::StepFunctions::StateMachine');
-    expect(stateMachines).toMatchSnapshot();
+    const normalizedStateMachines = normalizeS3Keys(stateMachines);
+    expect(normalizedStateMachines).toMatchSnapshot();
   });
 
   test('should match snapshot for EventBridge rule', () => {
     const template = Template.fromStack(stack);
     const eventRules = template.findResources('AWS::Events::Rule');
-    expect(eventRules).toMatchSnapshot();
+    const normalizedEventRules = normalizeS3Keys(eventRules);
+    expect(normalizedEventRules).toMatchSnapshot();
   });
 
   test('should match snapshot for CloudWatch Log Group', () => {
     const template = Template.fromStack(stack);
     const logGroups = template.findResources('AWS::Logs::LogGroup');
-    expect(logGroups).toMatchSnapshot();
+    const normalizedLogGroups = normalizeS3Keys(logGroups);
+    expect(normalizedLogGroups).toMatchSnapshot();
   });
 
   test('should match snapshot for IAM roles and policies', () => {
     const template = Template.fromStack(stack);
     const iamRoles = template.findResources('AWS::IAM::Role');
     const iamPolicies = template.findResources('AWS::IAM::Policy');
-    expect({ roles: iamRoles, policies: iamPolicies }).toMatchSnapshot();
+    const normalizedIamResources = normalizeS3Keys({ roles: iamRoles, policies: iamPolicies });
+    expect(normalizedIamResources).toMatchSnapshot();
   });
 });
